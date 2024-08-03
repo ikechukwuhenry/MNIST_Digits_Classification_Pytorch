746138f1cfb84bf02e234c9753f52d9e5045cd97
Ikechukwu Michael <ikechukwuhenry@ymail.com>
2024-08-03 17:37:18 +0100

    first commmit

---
+++ MnistDigitsClassifcationPytorch.py
@@ -0,0 +1,61 @@
+# !wget www.di.ens.fr/~lelarge/MNIST.tar.gz
+# !tar -zxvf MNIST.tar.gz
+
+# import dependencies
+import torch
+from PIL import Image
+from torch import nn, save, load
+from torch.optim import Adam
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+
+# Loading Data
+transform = transforms.Compose([transforms.ToTensor()])
+train_dataset = datasets.MNIST(root="data", download=False, train=True, transform=transform)
+train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+
+# Define the image classifier model
+class ImageClassifier(nn.Module):
+
+    def __init__(self):
+        super(ImageClassifier, self).__init__()
+        self.conv_layers = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=3),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=3),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3),
+            nn.ReLU()
+        )
+
+        self.fc_layers = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear( 64 * 22 * 22, 10)
+        )
+
+    def forward(self, x):
+        x = self.conv_layers(x)
+        x = self.fc_layers(x)
+        return x
+        
+
+# Create an instance of the image classifier model
+mps_device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+classifier = ImageClassifier().to(mps_device)
+
+# Define the optimizer and loss function
+optimizer = Adam(classifier.parameters(), lr=0.001)
+loss_fn = nn.CrossEntropyLoss()
+
+# Train the model
+for epoch in range(10): # Train for 10 epochs
+    for images, labels in train_loader:
+        images, labels = images.to(mps_device), labels.to(mps_device)
+        optimizer.zero_grad()   # Reset gradients
+        ouputs = classifier(images)     # Forward pass
+        loss = loss_fn(ouputs, labels)  # Compute loss
+        loss.backward()     # Backward pass
+        optimizer.step()    # Update weights
+    
+    print(f"Epoch:{epoch} loss is {loss.item()}")
